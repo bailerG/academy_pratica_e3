78e67b975c15fa4bd4e0972a0ecde17b5a191150
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-23 15:39:00 -0300

    AP4 Concluida

--- ed3_ap4.dart
+++ ed3_ap4.dart
@@ -1,1 +1,31 @@
-void main() {}
+import 'dart:math';
+
+void main() {
+  final aleatorio = Random();
+
+  final listaNumeros =
+      List.generate(15, (index) => aleatorio.nextInt(5000) + 1);
+
+  converterNumeros(listaNumeros);
+}
+
+void converterNumeros(List<int> lista) {
+  lista.sort();
+
+  for (final numero in lista) {
+    print(
+        'Decimal: $numero, Binario: ${toBinario(numero)}, Octal: ${toOctal(numero)}, Hexadecimal: ${toHexadecimal(numero)}');
+  }
+}
+
+String toBinario(int numero) {
+  return numero.toRadixString(2);
+}
+
+String toOctal(int numero) {
+  return numero.toRadixString(8);
+}
+
+String toHexadecimal(int numero) {
+  return numero.toRadixString(16);
+}
