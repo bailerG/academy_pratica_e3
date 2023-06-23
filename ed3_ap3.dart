9fe06eca6c838ab611f40906b7c59bd3abb7c0a8
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-23 14:57:33 -0300

    AP3 Concluida

--- ed3_ap3.dart
+++ ed3_ap3.dart
@@ -1,1 +1,23 @@
-void main() {}
+import 'dart:math';
+
+void main() {
+  final aleatorio = Random();
+
+  final listaRaios =
+      List.generate(10, (index) => aleatorio.nextDouble() * 99 - 1);
+
+  for (final raio in listaRaios) {
+    final area = areaCirculo(raio);
+    final perimetro = perimetroCirculo(raio);
+    print(
+        "Raio: ${raio.toStringAsFixed(2)}, Area: ${area.toStringAsFixed(2)}, Perímetro: ${perimetro.toStringAsFixed(2)} ");
+  }
+}
+
+double areaCirculo(double raio) {
+  return (raio * raio) * pi;
+}
+
+double perimetroCirculo(double raio) {
+  return 2 * pi * raio;
+}
