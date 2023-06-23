e5ed10f877e2ee95e1a1c8eb8b873fb095598d7f
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-23 14:22:13 -0300

    AP2 Concluida

--- ed3_ap2.dart
+++ ed3_ap2.dart
@@ -1,1 +1,26 @@
-void main() {}
+import 'dart:math';
+
+void main() {
+  final resultadoAB = funcaoA(funcaoB);
+  final resultadoAC = funcaoA(funcaoC);
+
+  print("A(B) = $resultadoAB");
+  print("A(C) = $resultadoAC");
+}
+
+int funcaoA(int Function(int) funcaoEscolhida) {
+  final aleatorio = Random();
+
+  final resultado1 = funcaoEscolhida(aleatorio.nextInt(33));
+  final resultado2 = funcaoEscolhida(aleatorio.nextInt(33));
+
+  return resultado1 + resultado2;
+}
+
+int funcaoB(int numero) {
+  return numero * numero;
+}
+
+int funcaoC(int numero) {
+  return numero * 2;
+}
